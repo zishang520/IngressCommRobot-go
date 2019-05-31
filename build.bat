685f01f38ce301c7f1a79220522c48e03e2396e3
zishang520 <zishang520@gmail.com>
2019-05-31 10:02:41 +0800

    Update Build Script

--- build.bat
+++ build.bat
@@ -5,49 +5,49 @@ echo ========================
 echo build darwin_386/Ingress
 set GOOS=darwin
 set GOARCH=386
-go build -o bin/darwin_386/Ingress Ingress.go
+go build -ldflags "-s -w" -o bin/darwin_386/Ingress Ingress.go
 
 echo ========================
 echo build darwin_amd64/Ingress
 set GOOS=darwin
 set GOARCH=amd64
-go build -o bin/darwin_amd64/Ingress Ingress.go
+go build -ldflags "-s -w" -o bin/darwin_amd64/Ingress Ingress.go
 
 echo ========================
 echo build linux_386/Ingress
 set GOOS=linux
 set GOARCH=386
-go build -o bin/linux_386/Ingress Ingress.go
+go build -ldflags "-s -w" -o bin/linux_386/Ingress Ingress.go
 
 echo ========================
 echo build linux_amd64/Ingress
 set GOOS=linux
 set GOARCH=amd64
-go build -o bin/linux_amd64/Ingress Ingress.go
+go build -ldflags "-s -w" -o bin/linux_amd64/Ingress Ingress.go
 
 echo ========================
 echo build linux_arm/Ingress
 set GOOS=linux
 set GOARCH=arm
-go build -o bin/linux_arm/Ingress Ingress.go
+go build -ldflags "-s -w" -o bin/linux_arm/Ingress Ingress.go
 
 echo ========================
 echo build linux_arm64/Ingress
 set GOOS=linux
 set GOARCH=arm64
-go build -o bin/linux_arm64/Ingress Ingress.go
+go build -ldflags "-s -w" -o bin/linux_arm64/Ingress Ingress.go
 
 echo ========================
 echo build windows_386/Ingress.exe
 set GOOS=windows
 set GOARCH=386
-go build -o bin/windows_386/Ingress.exe Ingress.go
+go build -ldflags "-s -w" -o bin/windows_386/Ingress.exe Ingress.go
 
 echo ========================
 echo build windows_amd64/Ingress.exe
 set GOOS=windows
 set GOARCH=amd64
-go build -o bin/windows_amd64/Ingress.exe Ingress.go
+go build -ldflags "-s -w" -o bin/windows_amd64/Ingress.exe Ingress.go
 
 echo ========================
 echo Successful
